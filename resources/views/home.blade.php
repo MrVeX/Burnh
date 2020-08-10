20c4f869efcd72a07d20bd7af9f1ba68bc91ac8f
MrVeX <kolomeicev.aleksei@gmail.com>
2020-08-10 12:58:48 +0300

    +

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,19 +42,7 @@
 <div id="content" class="container">
     <h1 class="heading">Burning Hamsters — Фриланс и не только</h1>
     <div class="row">
-        <div class="card horizontal">
-            <div class="card-image">
-              <img src="{{Storage::url('public/img/mems.jpg')}}">
-            </div>
-            <div class="card-stacked">
-              <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information.</p>
-              </div>
-              <div class="card-action">
-                <a href="#">This is a link</a>
-              </div>
-            </div>
-          </div>
+
     </div>
 </div>
 
