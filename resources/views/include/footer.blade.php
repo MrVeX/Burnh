af379aa33ba74e05f756a2bb2363402130fa770b
MrVeX <kolomeicev.aleksei@gmail.com>
2020-08-10 16:48:24 +0300

    add font awesome

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -10,6 +10,9 @@
                     <div class="col s6"></div>
                     <div class="col s3">
                         <ul class="footer-links">
+                            <li><a class="waves-effect waves-light btn-flat" href="mailto:kolomeicev.aleksei@yandex.ru"><i class="fa fa-envelope-open-o" aria-hidden="true"></i></a></li>
+                            <li><a class="waves-effect waves-light btn-flat" href="https://wa.me/79291659429"><i class="fa fa-whatsapp" aria-hidden="true"></i></i></a></li>
+                            <li><a class="waves-effect waves-light btn-flat" href="https://t.me/MrVeXter"><i class="fa fa-telegram" aria-hidden="true"></i></a></li>
                             <li><a class="waves-effect waves-light btn-flat" href="{{ route('home') }}">Главная</a></li>
                             {{-- <li><a class="waves-effect waves-light btn-flat"
                                     href="{{ route('portfolio') }}">Портфолио</a></li>
--- resources/views/include/scripts.blade.php
+++ resources/views/include/scripts.blade.php
@@ -1,22 +1,22 @@
 @section('scripts')
 <script>
     // Временно выключается - подготовка к аутеннтификации
-    // var code1 = new Typed('.element1', {
-    //     stringsElement :'#code-1',
-    //     typeSpeed: 40,
-    //     showCursor: false});
-    // var code2 = new Typed('.element2', {
-    //     stringsElement :'#code-2',
-    //     typeSpeed: 40,
-    //     startDelay: 1200,
-    //     showCursor: false}); 
-    // var code3 = new Typed('.element3', {
-    //     stringsElement :'#code-3',
-    //     typeSpeed: 40,
-    //     startDelay: 2000,
-    //     showCursor: false}); 
-    // var code4 = new Typed('.element4', {
-    //     stringsElement :'#code-4',
-    //     typeSpeed: 40,
-    //     startDelay: 3000}); 
+    var code1 = new Typed('.element1', {
+        stringsElement :'#code-1',
+        typeSpeed: 40,
+        showCursor: false});
+    var code2 = new Typed('.element2', {
+        stringsElement :'#code-2',
+        typeSpeed: 40,
+        startDelay: 1200,
+        showCursor: false});
+    var code3 = new Typed('.element3', {
+        stringsElement :'#code-3',
+        typeSpeed: 40,
+        startDelay: 2000,
+        showCursor: false});
+    var code4 = new Typed('.element4', {
+        stringsElement :'#code-4',
+        typeSpeed: 40,
+        startDelay: 3000});
 </script>
